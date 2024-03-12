df11e8a936f7ef9feed097689709ebd01f154609
Temulen GPG <temulen@Temulens-MacBook-Pro.local>
2024-03-12 23:46:49 +0800

    last home

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const { Pool } = require("pg");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
 
 const pgConfig = {
   host: PGHOST,
   database: PGDATABASE,
   username: PGUSER,
   password: PGPASSWORD,
-  port: 5432,
+  port: PGPORT,
   ssl: {
     require: true,
   },
@@ -16,17 +23,19 @@ const pgConfig = {
 
 const pool = new Pool(pgConfig);
 
-async function getPgVersion() {
+app.get("/users", async (req, res) => {
   const client = await pool.connect();
-
   try {
-    const result = await client.query("SELECT version()");
-    console.log(result.rows[0]);
+    client.query("SELECT * FROM users");
   } catch (error) {
     console.log(error);
   } finally {
     client.release();
   }
-}
 
-getPgVersion();
+  res.status(200).send({ message: "success" });
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
